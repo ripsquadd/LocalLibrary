144b3d1443baf303591ab9265fd1a5123927feb4
ripsquadd <harwester1337@gmail.com>
2022-10-25 13:27:55 +0700

    added books & authors lists

--- locallibrary/locallibrary/catalog/urls.py
+++ locallibrary/locallibrary/catalog/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.index, name='index'),
+]
+urlpatterns += [
+    url(r'^$', views.index, name='index'),
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
+]
+urlpatterns += [
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+]
+
+urlpatterns += [
+    url(r'^author/$', views.AuthorListView.as_view(), name='authors'),
+    url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
--- locallibrary/locallibrary/catalog/views.py
+++ locallibrary/locallibrary/catalog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 
 def index(request):
@@ -22,6 +23,29 @@ def index(request):
     return render(
         request,
         'index.html',
-        context={'num_books': num_books,'num_genres': num_genres, 'num_instances': num_instances,
+        context={'num_books': num_books, 'num_genres': num_genres, 'num_instances': num_instances,
                  'num_instances_available': num_instances_available, 'num_authors': num_authors},
     )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        # В первую очередь получаем базовую реализацию контекста
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Добавляем новую переменную к контексту и инициализируем её некоторым значением
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
